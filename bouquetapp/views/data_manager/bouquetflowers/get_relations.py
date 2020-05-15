1e1561e079fea6664c55c3d557143f31460370b4
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-15 14:42:32 -0500

    Added comments to get_relations manager

--- bouquetapp/views/data_manager/bouquetflowers/get_relations.py
+++ bouquetapp/views/data_manager/bouquetflowers/get_relations.py
@@ -4,7 +4,11 @@ from bouquetapp.models import model_factory, BouquetFlower
 
 def get_relations(bouquet_id):
     """
+    Grabs the relationships in the database that has the passed in bboquet id
 
+    Uses the BouquetFlower model to represent data
+    
+    => List BouquetFlower classes
     """
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = model_factory(BouquetFlower)
