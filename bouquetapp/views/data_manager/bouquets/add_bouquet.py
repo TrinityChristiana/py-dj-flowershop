48ad8c886ba267349828a8c3c3d37891061c0387
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-15 13:50:34 -0500

    added_bouquet function completed

--- bouquetapp/views/data_manager/bouquets/add_bouquet.py
+++ bouquetapp/views/data_manager/bouquets/add_bouquet.py
@@ -1,6 +1,19 @@
+import sqlite3
+from bouquetapp.views.connection import Connection
 
 def add_bouquet(form_data):
     """
-
+    Takes in form data
+    Runs INSERT sql query to add new boquet to database,
+    needs form_data["name"], form_data["occasion"]
+    => id of created bouquet
     """
-    pass
+    with sqlite3.connect(Connection.db_path) as conn:
+
+        db_cursor = conn.cursor()
+        return db_cursor.execute("""
+        INSERT INTO bouquetapp_bouquet(name, occasion)
+        VALUES(?, ?)
+        """, (form_data["name"], form_data["occasion"])).lastrowid
+
+        
