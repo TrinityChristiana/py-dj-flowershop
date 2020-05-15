11da62beb7e6fa6e67bc6e7fae41dd196ba6e0e7
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-15 15:53:31 -0500

    added path to bouquet_flower in urls, added remove_relation function

---
+++ bouquetapp/views/bouquet_flower/bouquets_flower_details.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect, reverse
+from ..data_manager import *
+
+def details_bouquets_flower(request, bouquet_flower_id, bouquet_id):
+    """
+    This view handels only POST request for bouquets_flower
+    """
+    if request.method == "POST":
+        form_data = request.POST
+        if "actual_method" in form_data and form_data["actual_method"] == "DELETE":
+            remove_relation(bouquet_flower_id)
+            return redirect("bouquetapp:bouquet", bouquet_id=bouquet_id)
+
--- bouquetapp/views/data_manager/bouquetflowers/__init__.py
+++ bouquetapp/views/data_manager/bouquetflowers/__init__.py
@@ -1,1 +1,2 @@
 from .get_relations import get_relations
+from .remove_relation import remove_relation
