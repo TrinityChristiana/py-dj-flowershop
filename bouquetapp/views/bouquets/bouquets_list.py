40f4cb8f9a443fe3b95103c9d350e8941cbad366
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-15 11:12:01 -0500

    created get_all_bouquets function

--- bouquetapp/views/bouquets/bouquets_list.py
+++ bouquetapp/views/bouquets/bouquets_list.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from ..data_manager import get_all_bouquets
 
 def list_bouquets(request):
     if request.method == "GET":
+        bouquets = get_all_bouquets()
         template = "bouquets/bouquets_list.html"
-        context = {}
+        context = {
+            "bouquets": bouquets
+        }
         return render(request, template, context)
---
+++ bouquetapp/views/data_manager/bouquets/get_all_bouquets.py
@@ -0,0 +1,17 @@
+import sqlite3
+from bouquetapp.views.connection import Connection
+from bouquetapp.models import model_factory, Bouquet
+
+def get_all_bouquets():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Bouquet)
+
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            *
+        FROM bouquetapp_bouquet
+        """)
+        
+    return db_cursor.fetchall()
